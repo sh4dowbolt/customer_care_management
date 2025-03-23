2b9db270dc016b4efa812526b290b6b05950ab5e
sh4dowbolt <dmit.palckin@yandex.ru>
2025-03-23 23:33:17 +0400

    add lombok to project, modify userEntity and UserDto, add application properties

---
+++ OrderService/src/main/java/com/suraev/dto/User.java
@@ -0,0 +1,10 @@
+package com.suraev.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer id;
+    String name;
+    String type;
+}
--- OrderService/src/main/java/com/suraev/entity/UserEntity.java
+++ OrderService/src/main/java/com/suraev/entity/UserEntity.java
@@ -1,12 +1,22 @@
 package com.suraev.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Table(name = "users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id",nullable = false)
     private Integer id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "type", nullable = false)
     private String type;
+
 }
