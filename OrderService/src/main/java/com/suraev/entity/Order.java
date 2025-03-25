376e978f52b0ab3fce455231e647226a2812d978
sh4dowbolt <dmit.palckin@yandex.ru>
2025-03-26 00:58:53 +0400

    Add order entity

---
+++ OrderService/src/main/java/com/suraev/entity/Order.java
@@ -0,0 +1,37 @@
+package com.suraev.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ordr_id", nullable = false)
+    private Integer id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @Column(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @Column(name = "product_id")
+    private Product product;
+
+    @Column(name = "total_price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalPrice;
+
+    @Column(name="create_date")
+    private Instant createdAt;
+
+
+}
