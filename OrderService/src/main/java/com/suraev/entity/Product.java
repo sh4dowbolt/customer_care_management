698bf6a7d72d6bd6ff04941bee4aa819099e2c29
sh4dowbolt <dmit.palckin@yandex.ru>
2025-03-26 11:53:57 +0400

    Add product entity

--- OrderService/src/main/java/com/suraev/entity/Product.java
+++ OrderService/src/main/java/com/suraev/entity/Product.java
@@ -1,4 +1,29 @@
 package com.suraev.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
 public class Product {
+
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "category")
+    private String category;
+
 }
