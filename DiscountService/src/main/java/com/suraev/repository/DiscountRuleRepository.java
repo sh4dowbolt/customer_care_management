49f28ca861085d39a8b1c30ad76f5e9e6df825ab
sh4dowbolt <dmit.palckin@yandex.ru>
2025-04-03 11:59:36 +0400

    add DiscountRuleRepository

---
+++ DiscountService/src/main/java/com/suraev/repository/DiscountRuleRepository.java
@@ -0,0 +1,8 @@
+package com.suraev.repository;
+
+import com.suraev.entity.DiscountRule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiscountRuleRepository extends JpaRepository<DiscountRule, Integer> {
+
+}
