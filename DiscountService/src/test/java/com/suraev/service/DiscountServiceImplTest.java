c4e50144845f8fcc7b7efcca7f0ffd6a5ff2586f
sh4dowbolt <dmit.palckin@yandex.ru>
2025-04-05 20:19:03 +0400

    test coverage for DiscountServiceImplTest

--- DiscountService/src/test/java/com/suraev/service/DiscountServiceImplTest.java
+++ DiscountService/src/test/java/com/suraev/service/DiscountServiceImplTest.java
@@ -7,16 +7,12 @@ import com.suraev.entity.enums.DiscountType;
 import com.suraev.entity.enums.ProductCategory;
 import com.suraev.entity.enums.UserType;
 import com.suraev.repository.DiscountRuleRepository;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -28,30 +24,91 @@ class DiscountServiceImplTest {
     @Mock
     private DiscountRuleRepository discountRuleRepository;
     @InjectMocks
-    private DiscountServiceImpl discountService;
+    private  DiscountServiceImpl discountService;
 
     @Test
-    public void shouldReturnNonNullDiscountValue() {
+    public void shouldReturnPercentDiscountValue() {
         // ('INDIVIDUAL', 'CLOTHES', 10000, 10, 'PERCENT')
         //given
-        DiscountRequest discountRequest = new DiscountRequest(UserType.INDIVIDUAL, ProductCategory.CLOTHES,
-                BigDecimal.valueOf(10000));
+        DiscountRequest discountRequest = getPercentDiscountRequest();
 
-        DiscountRule ruleFromDB = DiscountRule.builder().discountType(DiscountType.PERCENT).userType(UserType.INDIVIDUAL)
-                .discountValue(BigDecimal.valueOf(10)).productCategory(ProductCategory.CLOTHES).build();
+        DiscountRule ruleFromDB = getPersonDiscountRule();
 
         Optional<DiscountRule> discountRule = Optional.of(ruleFromDB);
 
-        BigDecimal expectedDiscount = BigDecimal.valueOf(10000).multiply(BigDecimal.valueOf(10)).divide(BigDecimal.valueOf(100));
+        BigDecimal expectedDiscount = getDiscountValueEqualOneThousand();
         //when
-        Mockito.when(discountRuleRepository.getDiscountAmount(discountRequest.userType(),discountRequest.productCategory(),
+        Mockito.when(discountRuleRepository.getDiscountAmount(discountRequest.userType(),
+                discountRequest.productCategory(),
                 discountRequest.price())).thenReturn(discountRule);
         //then
         DiscountResponse actualResult = discountService.calculateDiscountAmount(discountRequest);
 
-        assertThat(actualResult.discountAmount()).isPositive();
+        assertThat(actualResult.discountAmount()).isEqualByComparingTo(expectedDiscount);
+    }
+
+    @Test
+    public void shouldReturnFixedDiscountValue() {
+        //('JURIDICAL', 'ANY', 20000, 3000, 'FIXED')
+        //given
+        DiscountRequest discountRequest = getFixedDiscountRequest();
+
+        DiscountRule discountRule = getFixedDiscountRule();
+
+        Optional<DiscountRule> optionalDiscountRule = Optional.of(discountRule);
+
+        BigDecimal expectedDiscount = BigDecimal.valueOf(3000);
+        //when
+        Mockito.when(discountRuleRepository.getDiscountAmount(discountRequest.userType(),
+                discountRequest.productCategory(),discountRequest.price())).thenReturn(optionalDiscountRule);
+        //then
+        DiscountResponse actualResult = discountService.calculateDiscountAmount(discountRequest);
+
         assertThat(actualResult.discountAmount()).isEqualByComparingTo(expectedDiscount);
 
+    }
+    @Test
+    public void shouldReturnNounDiscountValueIfNotRightDiscountRule() {
+        // ('VIP', 'ELECTRONIC', 5000, 20, 'PERCENT')
+        //given
+        DiscountRequest discountRequest = getDiscountForTooSmallPrice();
+
+        Optional<DiscountRule> notExistedDiscountRule = Optional.empty();
+        //when
+        Mockito.when(discountRuleRepository.getDiscountAmount(discountRequest.userType(),discountRequest.productCategory(),
+                discountRequest.price())).thenReturn(notExistedDiscountRule);
+        //then
+        DiscountResponse actualResult = discountService.calculateDiscountAmount(discountRequest);
+
+        assertThat(actualResult.discountAmount()).isZero();
+
+    }
+
+    private static DiscountRequest getDiscountForTooSmallPrice() {
+        return new DiscountRequest(UserType.VIP, ProductCategory.ELECTRONIC, BigDecimal.valueOf(1000));
+    }
+
+    private static DiscountRule getFixedDiscountRule() {
+        return DiscountRule.builder().discountValue(BigDecimal.valueOf(3000)).discountType(DiscountType.FIXED)
+                .userType(UserType.JURIDICAL).build();
+    }
+
+    private static DiscountRequest getFixedDiscountRequest() {
+        return new DiscountRequest(UserType.JURIDICAL, ProductCategory.ANY, BigDecimal.valueOf(50000));
+    }
+
+
+    private static BigDecimal getDiscountValueEqualOneThousand() {
+        return BigDecimal.valueOf(10000).multiply(BigDecimal.valueOf(10)).divide(BigDecimal.valueOf(100));
+    }
+
+    private static DiscountRule getPersonDiscountRule() {
+        return DiscountRule.builder().discountType(DiscountType.PERCENT).userType(UserType.INDIVIDUAL)
+                .discountValue(BigDecimal.valueOf(10)).productCategory(ProductCategory.CLOTHES).build();
+    }
+    private static DiscountRequest getPercentDiscountRequest() {
+        return new DiscountRequest(UserType.INDIVIDUAL, ProductCategory.CLOTHES,
+                BigDecimal.valueOf(10000));
     }
 
 
