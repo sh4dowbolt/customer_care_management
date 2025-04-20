a4c1614ec5524a9a663ecf14af5ca637c5d623b4
sh4dowbolt <dmit.palckin@yandex.ru>
2025-04-20 22:01:49 +0400

    configuration eureka for modules

--- OrderService/src/main/java/com/suraev/OrderServiceRunner.java
+++ OrderService/src/main/java/com/suraev/OrderServiceRunner.java
@@ -2,8 +2,10 @@ package com.suraev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServiceRunner {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceRunner.class, args);
--- RegistryService/src/main/java/com/suraev/RegistryServiceRunner.java
+++ RegistryService/src/main/java/com/suraev/RegistryServiceRunner.java
@@ -2,12 +2,12 @@ package com.suraev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class DiscountServiceRunner {
+@EnableEurekaServer
+public class RegistryServiceRunner {
     public static void main(String[] args) {
-        SpringApplication.run(DiscountServiceRunner.class, args);
+        SpringApplication.run(RegistryServiceRunner.class);
     }
 }
